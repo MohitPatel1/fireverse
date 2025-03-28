744bd21386fcad210d293167b1cc3c28624e3a17
smarty <mohit.patel1966@gmail.com>
2025-03-28 18:36:48 +0530

    works offline with service worker.js file

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered:', registration);
+    })
+    .catch((error) => {
+      console.log('Service Worker registration failed:', error);
+    });
+}
+
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
