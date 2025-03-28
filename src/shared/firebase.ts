9232c7972cec4077d3d01840de09906de3b24de6
smarty <mohit.patel1966@gmail.com>
2025-03-28 18:31:34 +0530

    firebase offline done

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -1,14 +1,22 @@
-import { enableIndexedDbPersistence, getFirestore } from "firebase/firestore";
-
+import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
 import configs from "./configs";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 
-const firebaseApp = initializeApp(JSON.parse(configs.firebaseConfig));
+const app = initializeApp(JSON.parse(configs.firebaseConfig));
 
-export const auth = getAuth(firebaseApp);
-export const db = getFirestore(firebaseApp);
-export const storage = getStorage(firebaseApp);
+// Initialize Firestore with persistent cache
+const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({
+    tabManager: persistentMultipleTabManager()
+  })
+});
 
-enableIndexedDbPersistence(db, { forceOwnership: false });
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+// Initialize Storage
+const storage = getStorage(app);
+
+export { db, auth, storage };
